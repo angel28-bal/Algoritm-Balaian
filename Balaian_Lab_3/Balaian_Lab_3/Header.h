d29f66ba6c0a800a758e88958b101956a74afa67
angel28-bal <113028390+angel28-bal@users.noreply.github.com>
2022-12-21 12:10:05 +0300

    laba 3 &4

---
+++ Balaian_Lab_3/Balaian_Lab_3/Header.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+#include "pipeline.h"
+#include "gtn.h"
+#include "cs.h"
+#include "utility.h"
+#include "template_utility.h"
